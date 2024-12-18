2304ddfe7d0b5e75b00885114c707ba5632d29f3
Krish Arora <165298580+Krisha1708@users.noreply.github.com>
2024-12-18 17:41:19 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+let toDoList = ["Wake up", "Brush teeth", "Have a breakfast"];
+//printing the orignal array
+console.log(toDoList);
+// Output: ["Wake up", "Brush teeth", "Have a breakfast"]
+
+
+// adding a new task and printing an updated array
+toDoList.push("Go for a walk")
+console.log(toDoList);
+// Output: ["Wake up", "Brush teeth", "Have a breakfast", "Go for a walk"]
+
+
+// Removing the last task from an array and prinitng an updated array
+toDoList.pop();
+console.log(toDoList);
+// Output: ["Wake up", "Brush teeth", "Have a breakfast"]
